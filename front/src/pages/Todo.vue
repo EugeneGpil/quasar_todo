9fe49f191f8dc156655d7772c57aa38ac6118a9e
EugeneGpil <gpilligrim@gmail.com>
2022-10-11 18:33:09 +0300

    1

--- front/src/pages/Todo.vue
+++ front/src/pages/Todo.vue
@@ -1,10 +1,36 @@
 <template>
   <q-page class="bg-grey-3 column">
-    <h5>Todo</h5>
+    <q-list class="list" separator bordered>
+      <q-item v-for="(task, index) in tasks" :key="index" tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import {reactive} from 'vue'
+
+const tasks = reactive([
+  {
+    title: 'Open eyes',
+    done:  false,
+  },
+  {
+    title: 'Wake up',
+    done:  false,
+  },
+  {
+    title: 'Program',
+    done:  false,
+  },
+])
+</script>
 
 <script>
 import {defineComponent} from 'vue'
@@ -13,3 +39,9 @@ export default defineComponent({
   name: 'TodoPage',
 })
 </script>
+
+<style lang="scss" scoped>
+.list {
+  background-color: $main-background;
+}
+</style>
